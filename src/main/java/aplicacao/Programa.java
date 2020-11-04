1490620bab532d2444aeac98e4d38d26e62c8f0d
Andre Gomez <andregc1983@gmail.com>
2020-11-03 21:20:27 -0300

    Apagar objeto do banco de dados de uma entidade monitorada

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -13,11 +13,12 @@ public class Programa {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa"); // Instancia o "EntityManagerFactory" com as informações da classe "persistence".
 		EntityManager em = emf.createEntityManager(); // Instanciação que conecta ao banco de dados e contexto de persistência.
 		
-//------Função que busca um objeto pelo ID no banco de dados.
+//------Função para apagar objeto no banco de dados.
 		
-		Pessoa p = em.find(Pessoa.class, 2); // Pesquisa no BD, converte o dado em objeto e o instancia.
-		
-		System.out.println(p);
+		Pessoa p = em.find(Pessoa.class, 2); // Pesquisa no BD
+		em.getTransaction().begin(); // Sempre que for remover um objeto é necessário criar uma transação... 
+		em.remove(p); // Remove objeto do BD que esta MONITORADO. É um objeto que você acabou de inserir ou que buscou no BD.
+		em.getTransaction().commit(); //... e encerrá-la.
 		
 		System.out.println("Pronto!");
 		em.close(); // Fecha o Entity Manager depois de usá-lo.
