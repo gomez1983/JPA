8e03b465d31c9298f3ad4365e321bc7ab7a3aed8
Andre Gomez <andregc1983@gmail.com>
2020-11-03 21:12:02 -0300

    Método Find para buscar objetos no BD

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -9,20 +9,19 @@ import dominio.Pessoa;
 public class Programa {
 
 	public static void main(String[] args) {
-
-		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
-		Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
 		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa"); // Instancia o "EntityManagerFactory" com as informações da classe "persistence".
 		EntityManager em = emf.createEntityManager(); // Instanciação que conecta ao banco de dados e contexto de persistência.
 		
-		em.getTransaction().begin(); // Inicia uma transação no banco de dados.
-		em.persist(p1); // "persist" é o código que pega o objeto e o salva no banco de dados.
-		em.persist(p2); // "persist" é o código que pega o objeto e o salva no banco de dados.
-		em.persist(p3); // "persist" é o código que pega o objeto e o salva no banco de dados.
-		em.getTransaction().commit(); // Confirma a transação no banco de dados.
+//------Função que busca um objeto pelo ID no banco de dados.
+		
+		Pessoa p = em.find(Pessoa.class, 2); // Pesquisa no BD, converte o dado em objeto e o instancia.
+		
+		System.out.println(p);
+		
 		System.out.println("Pronto!");
-
+		em.close(); // Fecha o Entity Manager depois de usá-lo.
+		emf.close(); // Fecha o Entity Manager Factory depois de usá-lo.
+		
 	}
 }
